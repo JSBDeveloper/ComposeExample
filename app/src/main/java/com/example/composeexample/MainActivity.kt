1a65828ceae8879920898618ce5345eea7a170d2
JSBDeveloper <ids8600@naver.com>
2021-06-06 21:27:30 +0900

    ComposeExample - Android Studio Update

--- app/src/main/java/com/example/composeexample/MainActivity.kt
+++ app/src/main/java/com/example/composeexample/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.composeexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+            NewsStory()
+        }
+    }
+
+    @Composable
+    fun NewsStory() {
+        Column {
+            Text("A day in Shark Fin Cove")
+            Text("Davenport, California")
+            Text("December 2018")
+        }
+    }
+
+    @Preview
+    @Composable
+    fun DefaultPreview() {
+        NewsStory()
     }
 }
