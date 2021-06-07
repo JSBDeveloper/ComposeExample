c86ff15c074299638f30cf9b18a8cf6185fe0149
JSBDeveloper <ids8600@naver.com>
2021-06-08 00:45:03 +0900

    update compose

--- app/src/main/java/com/example/composeexample/MainActivity.kt
+++ app/src/main/java/com/example/composeexample/MainActivity.kt
@@ -3,10 +3,20 @@ package com.example.composeexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +29,30 @@ class MainActivity : AppCompatActivity() {
 
     @Composable
     fun NewsStory() {
-        Column {
-            Text("A day in Shark Fin Cove")
-            Text("Davenport, California")
-            Text("December 2018")
+        MaterialTheme {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+
+                Image(
+                    painter = painterResource(R.drawable.ic_launcher_background),
+                    contentDescription = null,
+                    modifier = Modifier.height(180.dp).fillMaxWidth()
+                        .clip(shape = RoundedCornerShape(4.dp)),
+                    contentScale = ContentScale.Crop
+                )
+
+                Spacer(Modifier.height(16.dp))
+
+                Text("A day wandering through the sandhills " +
+                        "in Shark Fin Cove, and a few of the " +
+                        "sights I saw",
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    style = typography.h6)
+                Text("Davenport, California", style = typography.body2)
+                Text("December 2018", style = typography.body2)
+            }
         }
     }
 
